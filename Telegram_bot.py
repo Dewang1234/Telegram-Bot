f47c29ca80d1d282455fe803b5bf007c39249fd0
Dewang1234 <dewangmahadadalkar05@gmail.com>
2026-03-20 22:44:54 +0530

    Update Telegram_bot.py

--- Telegram_bot.py
+++ Telegram_bot.py
@@ -3,7 +3,7 @@ from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTyp
 import os
 
 # 🔐 Use environment variable (IMPORTANT for Render)
-TOKEN = os.getenv("8704404848:AAE2ur5uiJ9zsnCeI99_2-RrrcHz4wDXjLQ")
+TOKEN ="8704404848:AAE2ur5uiJ9zsnCeI99_2-RrrcHz4wDXjLQ"
 
 # 📚 SUBJECT-BASED DATA
 links = {
